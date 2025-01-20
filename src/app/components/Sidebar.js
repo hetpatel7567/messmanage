e488c94c0d246a8db109d77f2d226147602e56d1
hetpatel7567 <hetp2758@gmail.com>
2025-01-21 02:42:46 +0530

    damn yay

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faChartBar, faTasks ,faDashboard } from '@fortawesome/free-solid-svg-icons';
 import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
     return (
         <div className={styles.sidebar}>
             <div className={styles.logo}>MessManage</div>
-            <hr className={styles.hr} />
-            <button className={styles.sidebarButton}>lawde</button>
-            <hr className={styles.hr} />
-            <button className={styles.sidebarButton}>Button 2</button>
-            <hr className={styles.hr} />
-            <button className={styles.sidebarButton}>Button 3</button>
+            <div className={styles.cbuttons}>
+                <button className={styles.dashboardButton}>
+                    <FontAwesomeIcon icon={faDashboard} className={styles.icon} />Dashboard
+                </button>
+                <hr className={styles.hr} />
+            </div>
+
+
+            <div className={styles.cbuttons}>
+            <button className={styles.sidebarButton}>
+                <FontAwesomeIcon icon={faChartBar} className={styles.icon} /> Button 2
+            </button>
+            <button className={styles.sidebarButton}>
+                <FontAwesomeIcon icon={faTasks} className={styles.icon} /> Button 3
+            </button>
+            <button className={styles.sidebarButton}>
+                <FontAwesomeIcon icon={faTasks} className={styles.icon} /> Button 3
+            </button>
+            <button className={styles.sidebarButton}>
+                <FontAwesomeIcon icon={faTasks} className={styles.icon} /> Button 3
+            </button>
+            <button className={styles.sidebarButton}>
+                <FontAwesomeIcon icon={faTasks} className={styles.icon} /> Button 3
+            </button>
+            
+            </div>
         </div>
     );
 };
